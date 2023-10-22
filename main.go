0792f67cb23abb4f905b0140748dc8cd36584cfb
Özgür Karakuş <103831584+Aagonn@users.noreply.github.com>
2023-10-22 22:59:02 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 /*
 //.................................DEĞİŞKENLER VE VERİ TİPLERİ........................\\
-// DEĞŞKENLER:
+
 func main() {
 	fmt.Print("merhaba ")
 	fmt.Print("dünya ")
@@ -13,7 +13,7 @@ func main() {
 	var metin string = "merhaba türkiye!" // "string" metin için kullanılır.
 	fmt.Println(metin)
 }
-//VERİ TİPLERİ:
+
 func main() {
 	var kdv int = 18 // "int" tam sayılar için kullanılır
 	fmt.Println(kdv)
